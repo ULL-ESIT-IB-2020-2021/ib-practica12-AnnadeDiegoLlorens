575301a06e726e22401f38c9b8e27a328816298d
Anna de Diego Llorens <alu0101441320@ull.edu.es>
2021-01-10 11:06:14 +0000

    --help de Fechas arreglado

--- ejercicios/Fechas/src/fecha_funciones.cc
+++ ejercicios/Fechas/src/fecha_funciones.cc
@@ -5,7 +5,7 @@
  * Informática Básica
  *
  * @author Anna de Diego Llorens
- * @date 6/01/2021
+ * @date 10/01/2021
  * @brief Archivo del programa Fecha  que explica al usuario 
  *        como se ha de usar el programa en caso de que este no 
  *        pase correctamente los comandos.
@@ -21,26 +21,31 @@
 using namespace std; 
 
 void Uso(int argc,char *argv[]){
-if(argc == 1){
-if (argc!=3){
-  string parameter{argv[1]};
-  if (parameter == "--help") {
-    cout << argv[0] << "-- Gestión de fechas " << endl << "Modo de uso:" << argv[0] 
-         << " fichero_entrada fichero_salida" << endl
-         << "Si el fichero se encuentra en una carpeta diferente del programa," << endl
-         << "asegurese de escribir correctamente el camino a seguir para llegar a los documentos." << endl
-         << " - fichero_entrada: Fichero con fechas escritas." << endl
-         << "Las fechas del fichero deben estar contenidas en multiples lineas " << endl
-         << "según el formato 'dd/mm/aa'." << endl
-         << " - fichero_salida:  Fichero con fechas ordenadas." << endl
-         << "Las fechas se ordenaran de forma ascendente, señalando aqueyas fechas de año bisiesto" << endl 
-         << "con una B colocada junto a la fecha." << endl;
-    exit(EXIT_SUCCESS);
-  }
-   cout << argv[0] << " - Gestión de fechas" << endl
-         << "Modo de uso: " << argv[0] << " fichero_entrada fichero_salida" << endl
-         << "Pruebe " << argv[0] << " --help para más información." << endl;
-    exit(EXIT_SUCCESS);
-}
-}
+ if ( argc != 3 ) {
+          
+          if ( argc != 2) {
+
+          cout << argv[0] << " - Gestión de fechas" << endl;
+          cout << "Modo de uso: " << argv[0] << " fichero_entrada fichero_salida" << endl
+               << "Pruebe " << argv[0] << " --help para más información." << endl;
+           exit(EXIT_SUCCESS);
+          }
+
+          string parameter = argv[1];
+          if ( parameter == "--help") {
+
+               cout << "argv[1]= " << argv[1];
+
+               cout << argv[0] << "--Gestión de fechas " << endl << "Modo de uso:" << argv[0] 
+                    << " fichero_entrada fichero_salida" << endl
+                    << endl 
+                    << " - fichero_entrada: Fichero con fechas escritas." << endl
+                    << "Las fechas del fichero deben estar contenidas en multiples lineas " << endl
+                    << "según el formato 'dd/mm/aa'." << endl
+                    << endl
+                    << " - fichero_salida:  Fichero con fechas ordenadas." << endl;
+               
+          exit(EXIT_SUCCESS);
+          }
+     }
 }
